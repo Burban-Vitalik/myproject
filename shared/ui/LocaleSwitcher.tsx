f4071ed05e4a5a0ed3a5ac608b82086931dad04d
vitaliiburban <burbanvitalik2002@gmail.com>
2026-03-31 19:32:46 +0300

    feat: set up shadcn & add select LocaleSwitcher

--- shared/ui/LocaleSwitcher.tsx
+++ shared/ui/LocaleSwitcher.tsx
@@ -1,29 +1,42 @@
 'use client';
 
-import { usePathname, useRouter } from '@/i18n/navigation';
 import { useLocale } from 'next-intl';
-// import styles from './LocaleSwticher.module.css';
+import { usePathname, useRouter } from '@/i18n/navigation';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const locales = [
+  { value: 'en', label: 'EN' },
+  { value: 'uk', label: 'UA' },
+  { value: 'ru', label: 'RU' },
+];
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
 
-  const switchLocale = (newLocale: string) => {
-    if (newLocale !== locale) {
-      router.replace(pathname, { locale: newLocale });
-      router.refresh();
-    }
+  const onSelectChange = (nextLocale: string) => {
+    router.replace(pathname, { locale: nextLocale });
   };
 
   return (
-    <select
-    //   className={styles.localeSelect}
-      value={locale}
-      onChange={e => switchLocale(e.target.value)}>
-      <option value="en">EN</option>
-      <option value="uk">UK</option>
-      <option value="ru">RU</option>
-    </select>
+    <Select value={locale} onValueChange={onSelectChange}>
+      <SelectTrigger className="w-[70px] border-none bg-transparent focus:ring-0 focus:ring-offset-0">
+        <SelectValue placeholder={locale.toUpperCase()} />
+      </SelectTrigger>
+      <SelectContent align="end">
+        {locales.map((loc) => (
+          <SelectItem key={loc.value} value={loc.value}>
+            {loc.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 }
